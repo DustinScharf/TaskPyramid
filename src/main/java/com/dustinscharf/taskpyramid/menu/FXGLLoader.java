a6d05928b1f42f5dc0a65c38b18f64e6329ec0e4
DustinScharf <d.scharf.2002@gmail.com>
2021-09-21 16:33:03 +0200

    Implemented toControlledScene

--- src/main/java/com/dustinscharf/taskpyramid/menu/FXGLLoader.java
+++ src/main/java/com/dustinscharf/taskpyramid/menu/FXGLLoader.java
@@ -1,4 +1,16 @@
 package com.dustinscharf.taskpyramid.menu;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+
+import java.io.IOException;
+
 public class FXGLLoader {
+    public static Scene toControlledScene(Menu controllerMenu, String fxglFilePath) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(FXGLLoader.class.getResource(fxglFilePath));
+
+        fxmlLoader.setController(controllerMenu);
+
+        return new Scene(fxmlLoader.load());
+    }
 }
