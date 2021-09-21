1be4a271a0fb02149d55b043a8028186cf5beed4
DustinScharf <d.scharf.2002@gmail.com>
2021-09-21 16:42:02 +0200

    Added JavaDoc

--- src/main/java/com/dustinscharf/taskpyramid/menu/Menu.java
+++ src/main/java/com/dustinscharf/taskpyramid/menu/Menu.java
@@ -3,5 +3,11 @@ package com.dustinscharf.taskpyramid.menu;
 import javafx.stage.Stage;
 
 public interface Menu {
+    /**
+     * Shows this menu on stage
+     *
+     * @param stage the stage to show this menu on
+     * @return true if the menu was shown successfully, false otherwise
+     */
     public boolean show(Stage stage);
 }
