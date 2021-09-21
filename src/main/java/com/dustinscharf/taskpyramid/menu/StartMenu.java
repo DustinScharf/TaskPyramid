680cae824da55c96e21ac0425c5bebae5edba727
DustinScharf <d.scharf.2002@gmail.com>
2021-09-21 16:55:17 +0200

    Added show call

--- src/main/java/com/dustinscharf/taskpyramid/menu/StartMenu.java
+++ src/main/java/com/dustinscharf/taskpyramid/menu/StartMenu.java
@@ -3,11 +3,12 @@ package com.dustinscharf.taskpyramid.menu;
 import javafx.stage.Stage;
 
 public class StartMenu implements Menu {
-    private static final String FXGL_FILE_PATH = ""/*TODO*/;
+    private static final String FXGL_FILE_PATH = "/StartMenu.fxml";
 
     @Override
     public boolean show(Stage stage) {
-        stage.setScene(FXGLLoader.toControlledScene(this, FXGL_FILE_PATH));
+        stage.setScene(new FXGLLoader().toControlledScene(this, FXGL_FILE_PATH));
+        stage.show();
         return true;
     }
 
