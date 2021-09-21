e8d2e7f92382b50298abab84de4ab0238d7397b9
DustinScharf <d.scharf.2002@gmail.com>
2021-09-21 16:42:31 +0200

    Added private static final String FXGL_FILE_PATH

--- src/main/java/com/dustinscharf/taskpyramid/menu/StartMenu.java
+++ src/main/java/com/dustinscharf/taskpyramid/menu/StartMenu.java
@@ -3,6 +3,8 @@ package com.dustinscharf.taskpyramid.menu;
 import javafx.stage.Stage;
 
 public class StartMenu implements Menu {
+    private static final String FXGL_FILE_PATH = ""/*TODO*/;
+
     @Override
     public boolean show(Stage stage) {
         return false; // TODO
