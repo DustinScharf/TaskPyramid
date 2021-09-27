d0f65e9c49c64bbeaa9dbafd97b34a47bf48b206
DustinScharf <d.scharf.2002@gmail.com>
2021-09-27 15:05:35 +0200

    Added taskPyramideHeadlineLabel @FXML

--- src/main/java/com/dustinscharf/taskpyramid/menu/StartMenu.java
+++ src/main/java/com/dustinscharf/taskpyramid/menu/StartMenu.java
@@ -2,10 +2,14 @@ package com.dustinscharf.taskpyramid.menu;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 public class StartMenu implements Menu {
     private static final String FXGL_FILE_PATH = "/fxgl/StartMenu.fxml";
+
+    @FXML
+    private Label taskPyramideHeadlineLabel;
 
     @FXML
     private Button playButton;
