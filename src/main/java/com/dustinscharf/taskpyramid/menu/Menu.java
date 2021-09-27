62bc67170d0a981dd2e726238b758964ef8b9fe2
DustinScharf <d.scharf.2002@gmail.com>
2021-09-27 15:32:36 +0200

    Removed init because this should be up to coder

--- src/main/java/com/dustinscharf/taskpyramid/menu/Menu.java
+++ src/main/java/com/dustinscharf/taskpyramid/menu/Menu.java
@@ -6,15 +6,10 @@ public interface Menu {
     /**
      * Shows this menu on stage
      * <p>
-     * This methode should (if necessary) call {@link Menu#init()}
+     * This methode should initialize all necessary components
      *
      * @param stage the stage to show this menu on
      * @return true if the menu was shown successfully, false otherwise
      */
     public boolean show(Stage stage);
-
-    /**
-     * Initializes all necessary components (like button listeners) for this menu
-     */
-    public void init();
 }
