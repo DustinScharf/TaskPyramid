cb940adb0b41a8210394dc6d63c843bf3eb8bb35
DustinScharf <d.scharf.2002@gmail.com>
2021-09-27 15:01:26 +0200

    Added play button @FXML

--- src/main/java/com/dustinscharf/taskpyramid/menu/StartMenu.java
+++ src/main/java/com/dustinscharf/taskpyramid/menu/StartMenu.java
@@ -1,9 +1,14 @@
 package com.dustinscharf.taskpyramid.menu;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 public class StartMenu implements Menu {
     private static final String FXGL_FILE_PATH = "/fxgl/StartMenu.fxml";
+
+    @FXML
+    private Button playButton;
 
     @Override
     public boolean show(Stage stage) {
