ffeccb19a993e2d4745c548fe8e9e73281edf7d9
DustinScharf <d.scharf.2002@gmail.com>
2021-09-27 15:18:52 +0200

    Added javadoc

--- src/main/java/com/dustinscharf/taskpyramid/menu/Menu.java
+++ src/main/java/com/dustinscharf/taskpyramid/menu/Menu.java
@@ -5,6 +5,8 @@ import javafx.stage.Stage;
 public interface Menu {
     /**
      * Shows this menu on stage
+     * <p>
+     * This methode should (if necessary) call {@link Menu#init()}
      *
      * @param stage the stage to show this menu on
      * @return true if the menu was shown successfully, false otherwise
