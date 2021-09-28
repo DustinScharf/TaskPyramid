634112d04cd1570983882938ddf8f7ed6c1d5462
DustinScharf <d.scharf.2002@gmail.com>
2021-09-28 04:53:07 +0200

    Implemented Task class

---
+++ src/main/java/com/dustinscharf/taskpyramid/game/task/Task.java
@@ -0,0 +1,13 @@
+package com.dustinscharf.taskpyramid.game.task;
+
+public class Task {
+    private String taskText;
+
+    public Task(String taskText) {
+        this.taskText = taskText;
+    }
+
+    public String getTaskText() {
+        return taskText;
+    }
+}
