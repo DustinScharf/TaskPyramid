2cb66542230682c9895016dd741c0aac8829c5c4
DustinScharf <d.scharf.2002@gmail.com>
2021-09-28 04:53:22 +0200

    Added basic components to game class

--- src/main/java/com/dustinscharf/taskpyramid/game/Game.java
+++ src/main/java/com/dustinscharf/taskpyramid/game/Game.java
@@ -1,4 +1,23 @@
 package com.dustinscharf.taskpyramid.game;
 
+import com.dustinscharf.taskpyramid.game.task.Tasks;
+
 public class Game {
+    private Tasks tasks;
+
+    public Game() {
+        this(new Tasks());
+    }
+
+    public Game(Tasks tasks) {
+        this.tasks = tasks;
+    }
+
+    public Tasks getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(Tasks tasks) {
+        this.tasks = tasks;
+    }
 }
