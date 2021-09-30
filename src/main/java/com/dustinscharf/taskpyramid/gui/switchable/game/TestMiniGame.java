6b01db86ff76994386563feb52f4da7d1af7e039
DustinScharf <d.scharf.2002@gmail.com>
2021-09-30 04:04:30 +0200

    Build basic TestMiniGame class structure

--- src/main/java/com/dustinscharf/taskpyramid/gui/switchable/game/TestMiniGame.java
+++ src/main/java/com/dustinscharf/taskpyramid/gui/switchable/game/TestMiniGame.java
@@ -1,4 +1,26 @@
 package com.dustinscharf.taskpyramid.gui.switchable.game;
 
-public class TestMiniGame {
+import com.dustinscharf.taskpyramid.gui.Switchable;
+import com.dustinscharf.taskpyramid.gui.util.FXGLLoader;
+import javafx.stage.Stage;
+
+public class TestMiniGame implements Switchable {
+    private static final String FXGL_FILE_PATH = "/fxgl/TestMiniGame.fxml";
+
+    @Override
+    public boolean show(Stage stage) {
+        stage.setScene(FXGLLoader.toControlledScene(FXGL_FILE_PATH, this));
+        this.init();
+        stage.show();
+        return true;
+    }
+
+    private void init() {
+        // TODO
+    }
+
+    //////////////////////
+    // Button Listeners //
+    //////////////////////
+    // TODO
 }
