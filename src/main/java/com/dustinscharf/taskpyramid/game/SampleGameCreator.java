668ddbe6ae449c7d12cc2669e8ced6e1cf8bf87a
DustinScharf <d.scharf.2002@gmail.com>
2021-09-30 16:01:12 +0200

    Added game return to create game methode

--- src/main/java/com/dustinscharf/taskpyramid/game/SampleGameCreator.java
+++ src/main/java/com/dustinscharf/taskpyramid/game/SampleGameCreator.java
@@ -4,12 +4,16 @@ import com.dustinscharf.taskpyramid.game.task.Task;
 import com.dustinscharf.taskpyramid.game.task.Tasks;
 
 public class SampleGameCreator {
-    public void createSampleGame() {
+    public static Game createSampleGame() {
         Game game = new Game();
+
         Tasks tasks = new Tasks();
         tasks.addTask(new Task("Go crazy"));
         tasks.addTask(new Task("Go insane"));
         tasks.addTask(new Task("Go eccentric"));
+
         game.setTasks(tasks);
+
+        return game;
     }
 }
