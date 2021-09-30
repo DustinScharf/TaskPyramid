e5281a306fbdc3edc42bd91565ee22b30878bfb3
DustinScharf <d.scharf.2002@gmail.com>
2021-09-30 15:35:18 +0200

    Created test game creation class

---
+++ src/main/java/com/dustinscharf/taskpyramid/game/SampleGameCreator.java
@@ -0,0 +1,15 @@
+package com.dustinscharf.taskpyramid.game;
+
+import com.dustinscharf.taskpyramid.game.task.Task;
+import com.dustinscharf.taskpyramid.game.task.Tasks;
+
+public class SampleGameCreator {
+    public void createSampleGame() {
+        Game game = new Game();
+        Tasks tasks = new Tasks();
+        tasks.addTask(new Task("Go crazy"));
+        tasks.addTask(new Task("Go insane"));
+        tasks.addTask(new Task("Go eccentric"));
+        game.setTasks(tasks);
+    }
+}
