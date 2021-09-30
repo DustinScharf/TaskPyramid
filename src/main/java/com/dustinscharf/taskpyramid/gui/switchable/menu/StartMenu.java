4cfa015cc42b6ba7076240e4b866a701de82c021
DustinScharf <d.scharf.2002@gmail.com>
2021-09-30 04:22:23 +0200

    Added css path as static variable

--- src/main/java/com/dustinscharf/taskpyramid/gui/switchable/menu/StartMenu.java
+++ src/main/java/com/dustinscharf/taskpyramid/gui/switchable/menu/StartMenu.java
@@ -3,12 +3,14 @@ package com.dustinscharf.taskpyramid.gui.switchable.menu;
 import com.dustinscharf.taskpyramid.gui.Switchable;
 import com.dustinscharf.taskpyramid.gui.util.FXGLLoader;
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 public class StartMenu implements Switchable {
     private static final String FXGL_FILE_PATH = "/fxgl/StartMenu.fxml";
+    private static final String CSS_FILE_PATH = "/css/StartMenu.css";
 
     @FXML
     private Label taskPyramideHeadlineLabel;
@@ -24,7 +26,9 @@ public class StartMenu implements Switchable {
 
     @Override
     public boolean show(Stage stage) {
-        stage.setScene(FXGLLoader.toControlledScene(FXGL_FILE_PATH, this));
+        Scene scene = FXGLLoader.toControlledScene(FXGL_FILE_PATH, this);
+        scene.getStylesheets().add(CSS_FILE_PATH);
+        stage.setScene(scene);
         this.init();
         stage.show();
         return true;
