616a2af8b6c351deec83964546aa8c0d890cfb00
DustinScharf <d.scharf.2002@gmail.com>
2021-09-30 04:46:23 +0200

    Added back button functionality

--- src/main/java/com/dustinscharf/taskpyramid/gui/switchable/game/TestMiniGame.java
+++ src/main/java/com/dustinscharf/taskpyramid/gui/switchable/game/TestMiniGame.java
@@ -1,26 +1,39 @@
 package com.dustinscharf.taskpyramid.gui.switchable.game;
 
-import com.dustinscharf.taskpyramid.gui.Switchable;
+import com.dustinscharf.taskpyramid.gui.switchable.Switchable;
+import com.dustinscharf.taskpyramid.gui.switchable.menu.StartMenu;
 import com.dustinscharf.taskpyramid.gui.util.FXGLLoader;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 public class TestMiniGame implements Switchable {
     private static final String FXGL_FILE_PATH = "/fxgl/TestMiniGame.fxml";
 
+    private Stage stage;
+
+    @FXML
+    private Button backButton;
+
     @Override
     public boolean show(Stage stage) {
-        stage.setScene(FXGLLoader.toControlledScene(FXGL_FILE_PATH, this));
+        this.stage = stage;
+        this.stage.setScene(FXGLLoader.toControlledScene(FXGL_FILE_PATH, this));
         this.init();
-        stage.show();
+        this.stage.show();
         return true;
     }
 
     private void init() {
+        this.backButton.setOnAction(actionEvent -> this.backButtonClicked());
         // TODO
     }
 
     //////////////////////
     // Button Listeners //
     //////////////////////
+    private void backButtonClicked() {
+        new StartMenu().show(this.stage);
+    }
     // TODO
 }
