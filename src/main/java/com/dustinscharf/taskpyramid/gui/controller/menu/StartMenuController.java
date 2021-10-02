75eaf1bd75c56ff607fec875b8d8ff8e5f96454f
DustinScharf <d.scharf.2002@gmail.com>
2021-10-03 01:32:41 +0200

    Added some tests for the scene (history) manager

--- src/main/java/com/dustinscharf/taskpyramid/gui/controller/menu/StartMenuController.java
+++ src/main/java/com/dustinscharf/taskpyramid/gui/controller/menu/StartMenuController.java
@@ -1,7 +1,15 @@
 package com.dustinscharf.taskpyramid.gui.controller.menu;
 
 import com.dustinscharf.taskpyramid.gui.controller.BaseController;
+import javafx.fxml.FXML;
 
 public class StartMenuController extends BaseController {
     // TODO
+
+    @FXML
+    private void playButtonClicked() {
+        super.sceneManager.switchScene("/fxml/game/Game.fxml");
+
+        super.sceneManager.switchToNextScene();
+    }
 }
