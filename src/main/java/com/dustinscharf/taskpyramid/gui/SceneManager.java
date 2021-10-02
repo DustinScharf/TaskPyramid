3ca2939b3218c4633c21ec93dccbbe25bc5824c0
DustinScharf <d.scharf.2002@gmail.com>
2021-10-03 01:09:37 +0200

    Added scene history

--- src/main/java/com/dustinscharf/taskpyramid/gui/SceneManager.java
+++ src/main/java/com/dustinscharf/taskpyramid/gui/SceneManager.java
@@ -7,24 +7,29 @@ import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class SceneManager {
     private final Stage stage;
 
-    private final Map<String, Scene> scenes = new HashMap<>();
+    private final Map<String, Scene> scenes;
+
+    private final List<Scene> sceneHistoryList;
+    private final ListIterator<Scene> sceneHistoryListIterator;
 
     public SceneManager(Stage stage) {
         if (stage == null) {
             throw new IllegalArgumentException("Stage stage can not be null");
         }
+
+        this.scenes = new HashMap<>();
         this.stage = stage;
+        this.sceneHistoryList = new LinkedList<>();
+        this.sceneHistoryListIterator = this.sceneHistoryList.listIterator();
     }
 
-    // TODO
     public void switchScene(String fxmlFilePath) {
-        Scene scene = scenes.computeIfAbsent(fxmlFilePath, fxmlFilePathTemp -> {
+        Scene scene = this.scenes.computeIfAbsent(fxmlFilePath, fxmlFilePathTemp -> {
             FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlFilePathTemp));
             try {
                 Pane pane = loader.load();
@@ -35,6 +40,24 @@ public class SceneManager {
                 throw new RuntimeException(ioException);
             }
         });
-        stage.setScene(scene);
+        this.sceneHistoryList.add(scene);
+        this.sceneHistoryListIterator.next();
+        this.stage.setScene(scene);
+    }
+
+    private void switchToNextScene() {
+        if (!this.sceneHistoryListIterator.hasNext()) {
+            throw new NullPointerException("No next scene in scene history");
+        }
+
+        this.stage.setScene(this.sceneHistoryListIterator.next());
+    }
+
+    private void switchToNextPrevious() {
+        if (!this.sceneHistoryListIterator.hasPrevious()) {
+            throw new NullPointerException("No previous scene in scene history");
+        }
+
+        this.stage.setScene(this.sceneHistoryListIterator.previous());
     }
 }
