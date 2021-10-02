a51082cd43f80d7fd5f86b0c7cd714149daf079f
DustinScharf <d.scharf.2002@gmail.com>
2021-10-03 00:00:04 +0200

    Created BaseController as superclass for all controllers

---
+++ src/main/java/com/dustinscharf/taskpyramid/gui/controller/BaseController.java
@@ -0,0 +1,11 @@
+package com.dustinscharf.taskpyramid.gui.controller;
+
+import com.dustinscharf.taskpyramid.gui.SceneManager;
+
+public class BaseController {
+    private SceneManager sceneManager;
+
+    public void setSceneManager(SceneManager sceneManager) {
+        this.sceneManager = sceneManager;
+    }
+}
