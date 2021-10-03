2809b0d9984b81735740e760ccf5ca9ce8934fcc
DustinScharf <d.scharf.2002@gmail.com>
2021-10-03 15:37:17 +0200

    Implemented action listeners

--- src/main/java/com/dustinscharf/taskpyramid/gui/controller/menu/StartMenuController.java
+++ src/main/java/com/dustinscharf/taskpyramid/gui/controller/menu/StartMenuController.java
@@ -1,6 +1,8 @@
 package com.dustinscharf.taskpyramid.gui.controller.menu;
 
 import com.dustinscharf.taskpyramid.gui.controller.BaseController;
+import com.dustinscharf.taskpyramid.gui.util.Alerter;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
 public class StartMenuController extends BaseController {
@@ -9,5 +11,15 @@ public class StartMenuController extends BaseController {
     @FXML
     private void playButtonClicked() {
         super.sceneManager.switchScene("/fxml/game/Game.fxml");
+    }
+
+    @FXML
+    public void createGameButtonClicked() {
+        Alerter.sendInfoAlert("Only pre created games are supported yet.");
+    }
+
+    @FXML
+    public void exitButtonClicked() {
+        super.sceneManager.getStage().close();
     }
 }
