d1d5ef1ef382b51e667b986d027c22463ebb6302
DustinScharf <d.scharf.2002@gmail.com>
2021-10-03 15:46:11 +0200

    Implemented exit action listener of GameController

--- src/main/java/com/dustinscharf/taskpyramid/gui/controller/game/GameController.java
+++ src/main/java/com/dustinscharf/taskpyramid/gui/controller/game/GameController.java
@@ -1,7 +1,16 @@
 package com.dustinscharf.taskpyramid.gui.controller.game;
 
+import com.dustinscharf.taskpyramid.game.SampleGameCreator;
 import com.dustinscharf.taskpyramid.gui.controller.BaseController;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
 public class GameController extends BaseController {
     // TODO
+
+    @FXML
+    public void exitButtonClicked() {
+        super.sceneManager.switchToPreviousScene();
+    }
 }
