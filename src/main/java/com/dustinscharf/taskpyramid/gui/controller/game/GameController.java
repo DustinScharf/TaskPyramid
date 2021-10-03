78c31bb0ee754053be7ba197061c26f26907be55
DustinScharf <d.scharf.2002@gmail.com>
2021-10-03 15:46:49 +0200

    Added old commented out code to reimplement with the new scene manager approach

--- src/main/java/com/dustinscharf/taskpyramid/gui/controller/game/GameController.java
+++ src/main/java/com/dustinscharf/taskpyramid/gui/controller/game/GameController.java
@@ -13,4 +13,16 @@ public class GameController extends BaseController {
     public void exitButtonClicked() {
         super.sceneManager.switchToPreviousScene();
     }
+
+    // TODO old init:
+//    private void init() {
+//        this.game = SampleGameCreator.createSampleGame();
+//
+//        int taskAmount = this.game.getTasks().getAmount();
+//        for (int taskNumber = 0; taskNumber < taskAmount; ++taskNumber) {
+//            Button button = new Button("Task " + (taskNumber + 1));
+//            // TODO add task button listeners
+//            this.taskButtonsHBox.getChildren().add(button);
+//        }
+//    }
 }
